2d1cf2a09512e388334f28dd76cbd64695990c86
Baptiste Rosillon <batros.web@gmail.com>
2017-04-11 09:54:01 +0200

    created tileset trap

---
+++ tiled/tiles/platform.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="32" tileheight="32" tilecount="20" columns="10">
+ <image source="../raw/tileset.png" width="320" height="64"/>
+</tileset>
