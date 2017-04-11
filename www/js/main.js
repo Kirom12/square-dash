681f95f0e0d7a547cf84dc296b24a4094e752a27
Baptiste Rosillon <batros.web@gmail.com>
2017-04-11 16:44:08 +0200

    added collision and keys

--- www/js/main.js
+++ www/js/main.js
@@ -3,13 +3,24 @@
  */
 
 //Global variables
-var gameInfo = {
+const gameInfo = {
 	div : 'game',
 	width : 1200,
 	height : 640,
 	initWidth : 3000,
 	initHeight : 3000,
 	scale : 0.5
+}
+
+const playerInfo = {
+	speed : 250,
+	jumpSpeed : 500,
+	startX : 10,
+	startY : gameInfo.height/2
+}
+
+const debug = {
+	godMode : false
 }
 
 var game = new Phaser.Game(gameInfo.initWidth, gameInfo.initHeight, Phaser.AUTO, gameInfo.div, {
@@ -19,9 +30,11 @@ var game = new Phaser.Game(gameInfo.initWidth, gameInfo.initHeight, Phaser.AUTO,
 	render: render
 });
 
-var map, layers, player;
 
+var map, layers, player;
+var buttons = {};
 var jumpTimer = 0;
+var currentColor = 0;
 
 //Game functions
 
@@ -29,7 +42,7 @@ var jumpTimer = 0;
  * Preload function
  */
 function preload() {
-	game.load.tilemap('map', 'assets/json/map.json', null, Phaser.Tilemap.TILED_JSON);
+	game.load.tilemap('map', 'assets/json/map-full.json', null, Phaser.Tilemap.TILED_JSON);
 	game.load.image('tileset', 'assets/img/tileset.png');
 	game.load.image('tileset-trap-new', 'assets/img/tileset-trap-new.png');
 	game.load.image('player', 'assets/img/player.png');
@@ -53,30 +66,44 @@ function create() {
 	map.addTilesetImage('tileset-trap-new');
 
 	layers = {
-		main : map.createLayer('main'),
-		main_collision : map.createLayer('collision-main')
+		base : {
+			main : map.createLayer('main'),
+			plateform_red : map.createLayer('plateform-red'),
+			plateform_green : map.createLayer('plateform-green'),
+			plateform_blue : map.createLayer('plateform-blue')
+		},
+		collision : {
+			main : map.createLayer('collision-main'),
+			die : map.createLayer('collision-die'),
+			collision_red : map.createLayer('collision-red'),
+			collision_green : map.createLayer('collision-green'),
+			collision_blue : map.createLayer('collision-blue')
+		}
 	}
 
-	layers.main.scale.set(0.5);
-	layers.main_collision.scale.set(1);
-	map.setCollisionBetween(1, 2000, true, layers.main_collision);
+	//Set scale on base layers
+	for (let i in layers.base) {
+		layers.base[i].setScale(gameInfo.scale);
+		layers.base[i].resizeWorld();
+	}
 
-	layers.main.resizeWorld();
+	//Set scale on collision layers
+	for (let i in layers.collision) {
+		map.setCollisionBetween(1, 200, true, layers.collision[i]);
+		layers.collision[i].setScale(gameInfo.scale);
+		layers.collision[i].alpha = 0;
+		layers.collision[i].resizeWorld();
+	}
 
-	layers.main_collision.alpha = 0.5;
-	layers.main_collision.resizeWorld();
-	
-	//map.setCollisionByExclusion([]);
-
-
-	// game.scale.setGameSize(gameInfo.width, gameInfo.height);
+	//Set game size
+	game.scale.setGameSize(gameInfo.width, gameInfo.height);
 
 
 	//Player
-	player = game.add.sprite(500, 500, 'player');
+	player = game.add.sprite(playerInfo.startX, playerInfo.startY, 'player');
 	game.physics.enable(player, Phaser.Physics.ARCADE);
 
-	player.scale.set(0.5);
+	player.scale.set(gameInfo.scale);
 
     player.body.collideWorldBounds = true;
 	player.body.bounce.y = 0;
@@ -85,7 +112,13 @@ function create() {
     game.camera.follow(player);
 
 	cursors = this.input.keyboard.createCursorKeys();
-	jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+	buttons = {
+		jump : game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR),
+		red : game.input.keyboard.addKey(Phaser.Keyboard.A),
+		green : game.input.keyboard.addKey(Phaser.Keyboard.Z),
+		blue : game.input.keyboard.addKey(Phaser.Keyboard.E)
+	}
 }
 
 /**
@@ -93,26 +126,57 @@ function create() {
  */
 function update() {
 
-	game.physics.arcade.collide(player, layers.main_collision, hit);
+	//Hide plateform
+	layers.base.plateform_red.alpha = 0.2;
+	layers.base.plateform_green.alpha = 0.2;
+	layers.base.plateform_blue.alpha = 0.2;
 
+	//Collisions
+	game.physics.arcade.collide(player, layers.collision.main);
+	game.physics.arcade.collide(player, layers.collision.die, playerHit);
+
+	//@TODO: refactor this switch
+	switch (currentColor) {
+		case 0 :
+			game.physics.arcade.collide(player, layers.collision.collision_red);
+			layers.base.plateform_red.alpha = 1;
+			break;
+		case 1 :
+			game.physics.arcade.collide(player, layers.collision.collision_green);
+			layers.base.plateform_green.alpha = 1;
+			break;
+		case 2 :
+			game.physics.arcade.collide(player, layers.collision.collision_blue);
+			layers.base.plateform_blue.alpha = 1;
+			break;
+		default:
+	}
+
+	//Actions
 	player.body.velocity.x = 0;
 
 	if (cursors.left.isDown) {
-		console.log("left");
-		player.body.velocity.x = -250;
+		player.body.velocity.x = -playerInfo.speed;
 	} else if (cursors.right.isDown) {
-		player.body.velocity.x = 250;
+		player.body.velocity.x = playerInfo.speed;
 	}
 
-	if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer) {
-		player.body.velocity.y = -600;
+	if (buttons.jump.isDown && player.body.onFloor() && game.time.now > jumpTimer) {
+		player.body.velocity.y = -playerInfo.jumpSpeed;
 		jumpTimer = game.time.now + 750;
 	}
+
+	//Switch color
+	if (buttons.red.isDown) {
+		currentColor = 0;
+	} else if (buttons.green.isDown) {
+		currentColor = 1;
+	} else if (buttons.blue.isDown) {
+		currentColor = 2;
+	}
 }
 
-function hit(){
-	console.log("hit");
-}
+
 /**
  * Render function
  */
@@ -120,4 +184,10 @@ function render() {
 
 }
 
-//Personnal functions
+//Collision functions
+function playerHit(player, world) {
+	if (debug.godMode) return;
+	
+	player.x = playerInfo.startX;
+	player.y = playerInfo.startY;
+}
